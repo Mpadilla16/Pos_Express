b1e06f3e0276b803a7d98458f80cbbb2d59d7258
saraypadilla <spadilla1079@gmail.com>
2025-05-07 21:34:40 -0500

    Js

--- controllers/sidebar.js
+++ controllers/sidebar.js
@@ -1,15 +1,9 @@
 
 const sidebar = document.getElementById('sidebar');
 const menuBtn = document.getElementById('menu-btn');
-const sidebarBtn = document.getElementById('sidebar-btn');
 
-sidebarBtn.addEventListener('click',()=>{
-    document.body.classList.toggle('sidebar-hidden');
-})
 
-menuBtn .addEventListener('click', ()=>{
+menuBtn.addEventListener('click', ()=>{
     sidebar.classList.toggle('minimize');
 });
 
-
- 
