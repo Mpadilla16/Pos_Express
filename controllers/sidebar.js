7077b0783378a6556fdd1f33b8dcfaa47f3d8059
saraypadilla <spadilla1079@gmail.com>
2025-05-07 22:29:45 -0500

    menu responsive

--- controllers/sidebar.js
+++ controllers/sidebar.js
@@ -1,7 +1,11 @@
 
 const sidebar = document.getElementById('sidebar');
 const menuBtn = document.getElementById('menu-btn');
+const sidebarBtn = document.getElementById('sidebar-btn');
 
+sidebarBtn.addEventListener('click',()=>{
+    document.body.classList.toggle('sidebar-hidden');
+})
 
 menuBtn.addEventListener('click', ()=>{
     sidebar.classList.toggle('minimize');
