b51d2fc461741c69a142815b97b9055b1d572885
saraypadilla <spadilla1079@gmail.com>
2025-05-07 20:33:57 -0500

    Menu lateral

--- controllers/sidebar.js
+++ controllers/sidebar.js
@@ -1,25 +1,15 @@
 
 const sidebar = document.getElementById('sidebar');
 const menuBtn = document.getElementById('menu-btn');
+const sidebarBtn = document.getElementById('sidebar-btn');
+
+sidebarBtn.addEventListener('click',()=>{
+    document.body.classList.toggle('sidebar-hidden');
+})
 
 menuBtn .addEventListener('click', ()=>{
     sidebar.classList.toggle('minimize');
 });
 
-menuItemsDropDown.forEach((menuItem) => {
-    menuItem.addEventListener('click', () => {
-        const subMenu = menuItem.querySelector('.sub-menu');
-        const isActive = menuItem.classList.toggle('sub-menu-toggle');
-        if (subMenu) {
-            if (isActive) {
-               
-                subMenu.style.height = `${subMenu.scrollHeight + 6}px`;
-                subMenu.style.padding = '0.2rem 0';
-            } else {
-                subMenu.style.height = '0';
-                subMenu.style.padding = '0';
-            }
-        }
-    });
-});
+
  
