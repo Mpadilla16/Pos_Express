8ecde60db3b6433c8dfc797ef20b1bdd587cf576
Mpadilla16 <mariaangpadilla@gmail.com>
2025-05-15 20:19:24 -0500

    corrije boton par sidebar en pantallas

--- frontend/controllers/sidebar.js
+++ frontend/controllers/sidebar.js
@@ -5,8 +5,12 @@ const sidebarBtn = document.getElementById('sidebar-btn');
 
 menuBtn.addEventListener('click', ()=>{
     sidebar.classList.toggle('minimize');
+    if (document.body.classList.toggle('sidebar-hidden')) {
+        sidebar.classList.toggle('minimize');
+    }
 });
 
 sidebarBtn.addEventListener('click',()=>{
     document.body.classList.toggle('sidebar-hidden');
-})
+}) 
+
